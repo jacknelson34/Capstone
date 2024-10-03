edfe86c27aa0bfa0fdb4b1d1eb30dd87c026de3e
Jack Nelson <89927092+jacknelson34@users.noreply.github.com>
2024-10-02 19:52:52 -0500

    Upload GrazeViewV1
    First upload

---
+++ UserInterface/GrazeViewV1/GrazeViewV1/MainPage.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrazeViewV1
+{
+    public partial class MainPage : Form
+    {
+        // Initialize Controls for use across multiple methods
+        private Label mainLabel;
+        private Button uploadButton;
+        private Button dataButton;
+        private LinkLabel userGuide;
+        private Button returntoWelcome;
+
+        public MainPage()
+        {
+            // Initialize Form Properties
+            this.Text = "Main Page : Utilities";
+            this.Size = new Size(800, 600);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            // Initialize Temporary Label
+            mainLabel = new Label();
+            mainLabel.Text = "Main Page : Options Below";
+            mainLabel.Font = new Font("Times New Roman", 24, FontStyle.Bold);
+            mainLabel.AutoSize = true;
+            this.Controls.Add(mainLabel);
+
+            // Initialize Upload Button
+            uploadButton = new Button();
+            uploadButton.Text = "Upload New Data";
+            uploadButton.Font = new Font("Times New Roman", 12, FontStyle.Italic);
+            uploadButton.AutoSize = true;
+            uploadButton.Click += uploadButton_Click;
+            this.Controls.Add(uploadButton);
+
+            // Initialize User Guide Button
+            userGuide = new LinkLabel();
+            userGuide.Text = "User Guide";
+            userGuide.Font = new Font("Times New Roman", 10, FontStyle.Regular);
+            userGuide.Size = new Size(100, 50);
+            userGuide.AutoSize = true;
+            userGuide.Click += HelpLabel_Click;
+            this.Controls.Add(userGuide);  
+
+            // Initialize Data Viewer Button
+            dataButton = new Button();
+            dataButton.Text = "Data Viewer";
+            dataButton.Font = new Font("Times New Roman", 12, FontStyle.Italic);
+            dataButton.AutoSize = true;
+            dataButton.Click += dataButton_Click;
+            this.Controls.Add(dataButton);
+
+            // Initialize Return to Welcome Page Button
+            returntoWelcome = new Button();
+            returntoWelcome.Text = "Return to Welcome Page";
+            returntoWelcome.Font = new Font("Times New Roman", 10, FontStyle.Regular);
+            returntoWelcome.AutoSize = true;
+            returntoWelcome.Click += returnButton_Click;
+            this.Controls.Add(returntoWelcome);
+
+            CenterControls();  // Center Buttons/Labels when created
+            this.Resize += MainPage_Resize;  // Call Resize Method if form is resized
+        }
+
+        private void uploadButton_Click(object? sender, EventArgs e)  // Upload Button Clicked
+        {
+            // Image Upload Code here
+        }
+
+        private void dataButton_Click(object? sender, EventArgs e)  // Data Viewer Button Clicked
+        {
+            // Connect to Data Viewer
+        }
+
+        private void HelpLabel_Click(object? sender, EventArgs e)  // User Guide Clicked
+        {
+            // Connect to help page
+            UserGuide.ShowHelpGuide();  // Call Method to only allow one instance open at a time
+        }
+
+        private void returnButton_Click(object? sender, EventArgs e)  // Return button Clicked
+        {
+            WelcomePage welcomepage = new WelcomePage();
+            welcomepage.Show();
+            this.Hide();
+        }
+
+        private void CenterControls()  // Method for centering labels and buttons
+        {
+            // COME BACK TO THIS TO FIX POSITIONING!!!
+
+            // Center Page Label
+            mainLabel.Location = new Point(
+                (this.ClientSize.Width / 2) - (mainLabel.Width / 2),
+                (this.ClientSize.Height / 6));
+
+            // Center Upload Button (Based on Page Label's Height)
+            uploadButton.Location = new Point(
+                (this.ClientSize.Width / 2) - (uploadButton.Width / 2),
+                (this.ClientSize.Height - mainLabel.Height) / 3);
+
+            // Position Data Viewer Button to bottom right corner
+            dataButton.Location = new Point(
+                (this.ClientSize.Width / 2) - (dataButton.Width / 2),
+                (this.ClientSize.Height - mainLabel.Height) / 2);
+
+            // Center Help Guide Button
+            userGuide.Location = new Point(
+                (this.ClientSize.Width - 70),
+                (this.ClientSize.Height - 20));
+
+            // Center Return button to bottom
+            returntoWelcome.Location = new Point(
+                (this.ClientSize.Width / 2) - (returntoWelcome.Width / 2),
+                this.ClientSize.Height - 40);
+        }
+
+        private void MainPage_Resize(object? sender, EventArgs e)  
+        {
+            CenterControls();  // Call CenterControls when form is resized
+        }
+    }
+}
---
+++ UserInterface/GrazeViewV1/GrazeViewV1/UserGuide.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrazeViewV1
+{
+
+    public partial class UserGuide : Form
+    {
+        private static UserGuide? instance;
+
+        public UserGuide()
+        {
+
+            // Initialize User Guide Properties
+            this.Text = "User Guide";
+            this.Size = new Size(800, 600);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            // Initialize RichTextBox for the Help Guide
+            RichTextBox helpTextBox = new RichTextBox();
+            helpTextBox.Dock = DockStyle.Fill;
+            helpTextBox.ReadOnly = true;
+            helpTextBox.ScrollBars = RichTextBoxScrollBars.Vertical; // Ensure a vertical scrollbar
+            helpTextBox.Font = new Font("Arial", 12);
+            helpTextBox.Text = LoadHelpContent(); // Load help content here
+
+            this.Controls.Add(helpTextBox);
+        }
+
+        private string LoadHelpContent()
+        {
+            return @"
+User Guide Will be inserted here";
+        }
+
+        public static void ShowHelpGuide()  // Static method to show user guide
+        {
+            if (instance == null || instance.IsDisposed)
+            {
+                instance = new UserGuide();
+            }
+
+            if (!instance.Visible)
+            {
+                instance.Show();
+            }
+            else
+            {
+                instance.BringToFront(); // Bring the form to the front if it's already open
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // Hide the form instead of closing to allow reopening
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
