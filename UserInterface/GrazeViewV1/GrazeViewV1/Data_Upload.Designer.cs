b75b945ea24797fac15d7ae26b22c4c1f7bca43a
Nathan Scott <njscott01@tamu.edu>
2024-10-10 17:43:40 -0500

    initial commit

---
+++ UserInterface/GrazeViewV1/GrazeViewV1/Data_Upload.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace GrazeView_Design
+{
+    partial class Data_Upload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Data_Upload
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1383, 893);
+            Name = "Data_Upload";
+            Text = "Data_Upload";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
--- UserInterface/GrazeViewV1/GrazeViewV1/Program.cs
+++ UserInterface/GrazeViewV1/GrazeViewV1/Program.cs
@@ -1,4 +1,4 @@
-namespace GrazeViewV1
+namespace GrazeView_Design
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace GrazeViewV1
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new WelcomePage());
+            Application.Run(new Welcome());
         }
     }
 }
