b58554ed855647097926b5f34665edf3c8d2737b
Nathan Scott <njscott01@tamu.edu>
2024-10-14 19:03:24 -0500

    ignore2

--- UserInterface/GrazeViewV1/GrazeViewV1/obj/Debug/net8.0-windows/GrazeViewV1.AssemblyInfo.cs
+++ UserInterface/GrazeViewV1/GrazeViewV1/obj/Debug/net8.0-windows/GrazeViewV1.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrazeViewV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b75b945ea24797fac15d7ae26b22c4c1f7bca43a")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1db55f267477013a523151e67c7f15724dd6f18")]
->>>>>>> Stashed changes
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43f8a7441dc7549f24a14c4aa1bad97183316a72")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrazeViewV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrazeViewV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
