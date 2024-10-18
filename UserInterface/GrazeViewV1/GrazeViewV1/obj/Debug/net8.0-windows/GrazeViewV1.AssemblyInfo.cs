2e032a5e454e5e6c2eb47c1402e2ec7beee850d1
Jack Nelson <89927092+jacknelson34@users.noreply.github.com>
2024-10-18 12:28:31 -0500

    Add DataUpload page
    Adding dataupload page between main page and loading page

--- UserInterface/GrazeViewV1/GrazeViewV1/obj/Debug/net8.0-windows/GrazeViewV1.AssemblyInfo.cs
+++ UserInterface/GrazeViewV1/GrazeViewV1/obj/Debug/net8.0-windows/GrazeViewV1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrazeViewV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82d37f1408bbe5cb264e82707534c87b79cae734")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b89cf7cfbfe5789924e0126b539ba66e2412ddb")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrazeViewV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrazeViewV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
